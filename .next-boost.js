8d48005ac36e47ff0f0deee342e225ceb96a6e52
Torkhov <torhov.s@gmail.com>
2022-07-28 17:37:16 +0200

    [+] add Cache

---
+++ .next-boost.js
@@ -0,0 +1,11 @@
+// An example with default cache adapter: hybrid-disk-cache
+
+/** @type {import('@next-boost/next-boost/dist/types').HandlerConfig} */
+module.exports = {
+  rules: [
+    {
+      regex: '.*',
+      ttl: 10,
+    },
+  ]
+}
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     apps: [
         {
-            name: 'mediaplatform',
+            name: 'questplatform',
             exec_mode: 'cluster',
             instances: 1, // Or a number of instances
             script: 'npm',
             args: 'start',
             env: {
-                PORT: process.env.PORT ?? '3020',
+                PORT: '3020',
             }
         }
     ]
